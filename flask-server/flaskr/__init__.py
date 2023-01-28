2837dc2bf7fe80d1652aabce6d955bccda31789a
Auguste Janickaite <auguste@Augustes-MBP.lan>
2023-01-28 22:32:48 +0200

    modified init.py file and added static folder

--- flask-server/flaskr/__init__.py
+++ flask-server/flaskr/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 DATABASE_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../client/build", static_url_path="/")
     app.config['SECRET_KEY'] = "secretBlaKey-345"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
     db.init_app(app)
