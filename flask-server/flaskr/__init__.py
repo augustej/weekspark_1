bc184d611e320f46cc3be53a59c5d3cf36a0320e
Auguste Janickaite <auguste@Augustes-MBP.lan>
2023-01-29 18:15:12 +0200

    modified os.getenv to os.environ.get

--- flask-server/flaskr/__init__.py
+++ flask-server/flaskr/__init__.py
@@ -9,7 +9,8 @@ DATABASE_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__, static_folder="../client/build", static_url_path="/")
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    print(os.getenv('SECRET_KEY'), "ENV")
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('../../../') + f'{DATABASE_NAME}'
     db.init_app(app)
